a05481fc472f8a45c894789a281dfbfa83119e35
Serahwakaba <serahwakaba9@gmail.com>
2021-07-28 15:55:55 +0300

    create profile toast

--- app/src/main/java/com/example/cryptocurrencyinvesting/Welcome.java
+++ app/src/main/java/com/example/cryptocurrencyinvesting/Welcome.java
@@ -2,7 +2,9 @@ package com.example.cryptocurrencyinvesting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+    public void createPortfolio(View view) {
+
+        startActivity(new Intent(getApplicationContext(), Login.class));
+
     }
 }
