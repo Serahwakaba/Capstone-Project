9c7fe99845ab22d0897567b08bf16734c4961f11
Serahwakaba <serahwakaba9@gmail.com>
2021-07-29 10:48:34 +0300

    context commit

--- app/src/main/java/com/example/cryptocurrencyinvesting/MyCustomAdapter.java
+++ app/src/main/java/com/example/cryptocurrencyinvesting/MyCustomAdapter.java
@@ -1,4 +1,23 @@
 package com.example.cryptocurrencyinvesting;
 
-public class MyCustomAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class MyCustomAdapter extends ArrayAdapter<CurrencyModel> {
+    private Context context;
+    private List<CurrencyModel> currencyModelsList;
+    private List<CurrencyModel> currencyModelsListFiltered;
+
+
+    public MyCustomAdapter( Context context, List<CurrencyModel> currencyModelsList) {
+        super(context, R.layout.list_custom_item,currencyModelsList);
+
+        this.context = context;
+        this.currencyModelsList = currencyModelsList;
+        this.currencyModelsListFiltered = currencyModelsList;
+
+    }
+
 }
